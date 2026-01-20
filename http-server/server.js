5cfb865b1e733fbad8bc073aed5e6e722a36d1a1
ABRAR ALi <abraraliengineer@gmail.com>
2026-01-20 16:53:37 +0500

    install nodemon

--- http-server/server.js
+++ http-server/server.js
@@ -16,22 +16,26 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write("I am a software engineer");
+    res.setHeader("content-type","text/html")
+    res.write("<h1>I am a software engineer</h1>");
     res.end();
   }
   if (req.url === "/about") {
+      res.setHeader("content-type","text/html")
     res.write(
-      "Hi, I'm Abrar Ali, a passionate React.js Developer dedicated to building fast, interactive, and user-friendly web applications. With a strong foundation in JavaScript and modern UI libraries, I specialize in turning complex problems into elegant, high-performance code. My goal is to create digital experiences that are not only functional but also visually engaging",
+      "<h1>Hi, I'm Abrar Ali, a passionate React.js Developer dedicated to building fast, interactive, and user-friendly web applications. With a strong foundation in JavaScript and modern UI libraries, I specialize in turning complex problems into elegant, high-performance code. My goal is to create digital experiences that are not only functional but also visually engaging</h1>",
     );
     res.end();
   }
   if (req.url === "/contact") {
-    res.write("Contact us");
+      res.setHeader("content-type","text/html")
+    res.write("<h1>Contact us for more information </h1>");
     res.end();
   }
 });
 
 const Port = 5000;
 server.listen(Port, () => {
+       
   console.log(`listening a port ${Port}`);
 });
