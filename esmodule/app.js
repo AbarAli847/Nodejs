464f3cdc5c16cb32886f5df75bba9d68220b7886
ABRAR ALi <abraraliengineer@gmail.com>
2026-01-21 12:10:40 +0500

    esmodule

---
+++ esmodule/app.js
@@ -0,0 +1,17 @@
+//  const user = {
+//     name: 'Abrar ali Engineer',
+//     age : 22,
+//  }
+
+//  module.exports = user
+
+  const add =  (a,b) => {
+   return  a+b
+}
+const sub = (a,b) => a-b
+
+export  {add,sub} ;
+
+
+
+// jab multi export karna ha tu export ke sath default use ni karna 
---
+++ esmodule/math.js
@@ -0,0 +1,9 @@
+// const calc = require('./app')
+
+import {add,sub} from "./app.js"
+
+// import { add } from "./app";
+
+
+ console.log(add(100,2000))
+ console.log(sub(300,137))
